f333abe4d776cf3acf3a1d36fb2b74f42b4c785a
achasek <chaseknieper@gmail.com>
2023-11-02 09:55:38 -0700

    cleaner code

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 let count = 0;
 
+// const bulbContainer = document.querySelector('.row');
+
 const bulb1 = document.querySelector('#lightbulb1');
 const bulb2 = document.querySelector('#lightbulb2');
 const bulb3 = document.querySelector('#lightbulb3');
 
 const output = document.querySelector('.subtitle');
 
+// bulbsContainer.addEventListener('click', (event) => {
+//     if (event.target.classList.contains('item')) {
+//         count++;
+//         output.innerHTML = `You've clicked the lights ${count} time${count === 1 ? '' : 's'}`;
+//         event.target.classList.toggle('active');
+//     }
+// });
+
 bulb1.addEventListener('click', () => {
     count++;
-    output.innerHTML = "You've clicked the lights " + count + " times";
+    output.innerHTML = `You've clicked the lights ${count} time${count === 1 ? '' : 's'}`
     bulb1.classList.toggle('active');
 });
 
 bulb2.addEventListener('click', () => {
     count++;
-    output.innerHTML = "You've clicked the lights " + count + " times";
+    output.innerHTML = `You've clicked the lights ${count} time${count === 1 ? '' : 's'}`
     bulb2.classList.toggle('active');
 });
 
 bulb3.addEventListener('click', () => {
     count++;
-    output.innerHTML = "You've clicked the lights " + count + " times";
+    output.innerHTML = `You've clicked the lights ${count} time${count === 1 ? '' : 's'}`
     bulb3.classList.toggle('active');
+});
+
+bulb1.addEventListener('mouseover', (event) => {
+    event.target.classList.toggle('active');
+});
+
+bulb2.addEventListener('mouseover', (event) => {
+    event.target.classList.toggle('active');
+});
+
+bulb3.addEventListener('mouseover', (event) => {
+    event.target.classList.toggle('active');
 });
