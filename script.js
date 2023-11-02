afb5783dd1ddf9a3815fd2e8cb8995c9f55ca9ed
achasek <chaseknieper@gmail.com>
2023-11-02 09:27:29 -0700

    finished

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your code here
+let count = 0;
+
+const bulb1 = document.querySelector('#lightbulb1');
+const bulb2 = document.querySelector('#lightbulb2');
+const bulb3 = document.querySelector('#lightbulb3');
+
+const output = document.querySelector('.subtitle');
+
+bulb1.addEventListener('click', () => {
+    count++;
+    output.innerHTML = "You've clicked the lights " + count + " times";
+    bulb1.classList.toggle('active');
+});
+
+bulb2.addEventListener('click', () => {
+    count++;
+    output.innerHTML = "You've clicked the lights " + count + " times";
+    bulb2.classList.toggle('active');
+});
+
+bulb3.addEventListener('click', () => {
+    count++;
+    output.innerHTML = "You've clicked the lights " + count + " times";
+    bulb3.classList.toggle('active');
+});
